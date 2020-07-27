8d1c333bc5c586682a91fa1add559f3f39a63646
Daniel Vena <deaconvena+github@gmail.com>
2020-07-27 18:33:47 -0400

    add alternate fullwidth selector

--- build/frontend.asset.php
+++ build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => '97e5440a15d21d66ecd9062460ee6f27');
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'f93481d71ca718ff5325a7b1a80f33e3');
